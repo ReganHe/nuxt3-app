4ea8139ad0591e71795bc46393248a8afccce0c9
Mark He <heke@hujiang.com>
2022-10-19 23:07:46 +0800

    Init

---
+++ nuxt3app.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nuxt3-app',
+      // exec_mode: 'cluster',
+      instances: '1',
+      script: './.output/server/index.mjs',
+      // env: {
+      //   PORT: 3333,
+      //   NODE_ENV: 'development',
+      // },
+    },
+  ],
+};
