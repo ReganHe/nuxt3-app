a984a9868715a23d691a38bc3bf9a171e837ce74
Mark He <heke@hujiang.com>
2022-10-20 23:00:35 +0800

    1. 集成prettier 2. 集成tailwinds

--- app.vue
+++ app.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-blue-600">
+        {{ brand }}
+      </h1>
+      <p class="mt-4 text-9xl font-bold text-gray-900">
+        {{ title }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 const year: number = 2022
-const title: string = `${year} iThome 铁人赛`
+const brand: string = `${year} iThome`
+const title: string = '铁人赛'
 </script>
