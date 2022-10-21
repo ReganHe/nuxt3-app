3f137fb2169d2b61372593e092a29ef6fd64f639
Mark He <heke@hujiang.com>
2022-10-21 13:26:42 +0800

    路由实践

--- app.vue
+++ app.vue
@@ -1,18 +1,5 @@
 <template>
-  <div class="bg-white py-24">
-    <div class="flex flex-col items-center">
-      <h1 class="text-6xl font-semibold text-blue-600">
-        {{ brand }}
-      </h1>
-      <p class="mt-4 text-9xl font-bold text-gray-900">
-        {{ title }}
-      </p>
-    </div>
+  <div>
+    <NuxtPage />
   </div>
 </template>
-
-<script lang="ts" setup>
-const year: number = 2022
-const brand: string = `${year} iThome`
-const title: string = '铁人赛'
-</script>
